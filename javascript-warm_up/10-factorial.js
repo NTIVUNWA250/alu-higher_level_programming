3d590aa30b1aae92f6aaffb60ec8927e3645e3de
Gilbert <your_email@example.com>
2025-09-27 02:40:13 -0700

    5-10

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+function fac (n) {
+  if (n < 0) {
+    return ("Not applicable")
+  } else if (n == 0 || Number.isNaN(n) || n == 1) {
+    return 1;
+  } else {
+    return fac(n - 1) * n;
+  }
+}
+
+const num = Number(process.argv[2]);
+console.log(fac(num));
