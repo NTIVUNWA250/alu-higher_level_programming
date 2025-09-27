9c38112901ee67ea4b3f48c57a389f9a57525453
Gilbert <your_email@example.com>
2025-09-27 01:36:13 -0700

    4

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
-if(process.argv.length -2 === 0){
-	console.log('No argument');
-}
-else {
-	console.log(process.argv[2]+' is '+process.argv[3]);
+if(process.argv.length -2 === 0) {
+  console.log('0 argument');
+} else {
+  console.log(process.argv[2]+' is '+process.argv[3]);
 }
